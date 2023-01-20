2ed767d37fb97ba2690518ad54ff58539e5f2c38
AndLego <galeon.gb@gmail.com>
2023-01-20 15:03:27 -0500

    added faker in order to generate random products(fake api) and a simplee xample of a CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,27 @@
-import { addProduct } from "./products/product.service";
+import { addProduct, products } from "./products/product.service";
+import { faker } from "@faker-js/faker"
 
-addProduct({
-  id: 1,
-  title: "Camisa",
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 2,
-  size: "L",
-  category: {
-    id: 23,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    name: "ropa"
-  }
-})
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.commerce.color(),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({ min: 10, max: 100 }),
+    size: faker.helpers.arrayElement(["M", "S", "L", "XL"]),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  })
+}
+
+console.log(products);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,17 @@ export const products: Product[] = []
 export const addProduct = (data: Product) => {
   products.push(data)
 }
+
+export const updateProduct = (id: string, element: Product) => {
+  let index = products.findIndex(item => item.id === id)
+  products.splice(index, 1, element)
+}
+
+export const deleteProduct = (id: string) => {
+  let index = products.findIndex(item => item.id === id)
+  products.splice(index, 1)
+}
+
+export const getProduct = (id: string) => {
+  //code
+}
